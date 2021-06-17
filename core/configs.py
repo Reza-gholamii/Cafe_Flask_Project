29ec89feeafe1b065ad9bd08c25d09b5faef9e67
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-17 21:46:32 +0430

    write enter and exit magic method for query

--- core/configs.py
+++ core/configs.py
@@ -12,23 +12,32 @@ logging.basicConfig(level=logging.INFO,
 class DataBaseContext:
     def __init__(self):
         try:
+            # establishing the connection
             conn: connection = connect(database="postgres",
                 user=dbconfig.get("user", "postgres"), password=dbconfig.get("password"),
                 host=dbconfig.get("host", "localhost"), port=dbconfig.get("port", "5432"))
 
             conn.autocommit = True
+            # creating a cursor object using the cursor() method
             curs: cursor = conn.cursor()
 
+            # preparing query to create a database
             SQL = f"CREATE DATABASE {dbconfig.get('dbname')};"
+            # creating a database
             curs.execute(SQL)
 
+            # closing the connection
             conn.close()
             logging.info(f"{__name__}: Database Created Successfully.")
         except:
             logging.warning(f"{__name__}: Database Already Existed.")
 
     def __enter__(self):
-        pass
+        self.conn: connection = connect(config)
+        self.curs: cursor = self.conn.cursor()
+        return self.curs
 
     def __exit__(self):
-        pass
+        self.conn.commit()
+        self.curs.close()
+        self.conn.close()
