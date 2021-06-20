71d1834dd35acc07792573448fa9d651162e1984
Reza Gh <r.msc.student93@gmail.com>
2021-06-20 15:49:28 +0430

    js code updated

--- static/js/order_list.js
+++ static/js/order_list.js
@@ -1,13 +1,17 @@
 function change_status(status) {
     let row_number = status.parentNode.parentNode
     let target_column = row_number.childNodes[5]
-    if (status.id === 'icon_cook') {
+    if (status.id === 'icon_cook')
+    {
         target_column.innerText = 'در حال پخت'
-    } else if (status.id === 'icon_serve') {
+    }
+    else if (status.id === 'icon_serve') {
         target_column.innerText = 'سرو شده'
-    } else if (status.id === 'icon_check') {
+    }
+    else if (status.id === 'icon_check') {
         target_column.innerText = 'پرداخت شده'
-    } else if (status.id === 'icon_trash') {
+    }
+    else if (status.id === 'icon_trash') {
         target_column.innerText = 'کنسل شده'
     }
 }
