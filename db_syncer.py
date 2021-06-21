3c69b1bd29d36c4ef6e5943eb31d3291f0ea43ff
mryazdan <mrrezayazdani@yahoo.com>
2021-06-21 11:47:23 +0430

    add configuration into db_syncer

--- db_syncer.py
+++ db_syncer.py
@@ -9,6 +9,8 @@ dbconfig = {
     "port": '5432'
 }
 
+config = " ".join([f"{key}={dbconfig[key]}" for key in dbconfig.keys()])
+
 # creating tables
 with DataBaseContext(dbconfig) as DBCursor:
     for query in sql_queries:
