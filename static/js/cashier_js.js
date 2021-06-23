e6e79340afe771328b1486de64d2a1ae636ac314
rassool-ahad <rasoolahad1997@gmail.com>
2021-06-23 22:52:58 +0430

    some bugs fixed

---
+++ static/js/cashier_js.js
@@ -0,0 +1,26 @@
+function hamburger_open(){
+    const menu = document.getElementById("nav_menu")
+    const close = document.getElementById("hamburger_close")
+    const open = document.getElementById("hamburger_open")
+    const hero= document.getElementById("menu-position")
+    open.style.display = "none"
+    close.style.visibility = "visible"
+    hero.appendChild(menu)
+    hero.style.display = "inline-block"
+    menu.style.display = "inline-block"
+
+}
+
+function hamburger_close(){
+    const navparent = document.getElementById("nav_parent")
+    const hero=  document.getElementById("nav_menu")
+    const menu = document.getElementById("menu-position")
+    const close = document.getElementById("hamburger_close")
+    const open = document.getElementById("hamburger_open")
+    open.style.removeProperty("display")
+    close.style.visibility = "hidden"
+    menu.style.display = "none"
+    navparent.appendChild(hero)
+
+}
+
