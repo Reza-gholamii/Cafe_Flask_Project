688dfcc7dc631ee5d62f070ce999e8547089144c
mryazdan <mrrezayazdani@yahoo.com>
2021-06-27 09:52:24 +0430

    add dashboard in html views

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.add_url_rule("/menu", 'menu', html.menu)
 app.add_url_rule("/contact_us", 'contact_us', html.contact_us, methods=['GET', 'POST'])
 app.add_url_rule("/about_us", 'about_us', html.about_us)
 
+app.add_url_rule("/cashier/", 'dashboard', html.dashboard, methods=['GET', 'POST'])
 app.add_url_rule("/cashier/orders", 'order_list', html.order_list, methods=['GET', 'POST'])
 app.add_url_rule("/cashier/menu", 'menu_items', html.menu_items, methods=['GET', 'POST'])
 app.add_url_rule("/cashier/orders/served", 'served_order_list', html.served_order_list, methods=['GET', 'POST'])
