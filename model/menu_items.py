4001bcb916fbc1dcb54c00ef6e4072e61825a8cf
Reza Gh <r.msc.student93@gmail.com>
2021-06-29 00:59:14 +0430

    status added to model

--- model/menu_items.py
+++ model/menu_items.py
@@ -17,11 +17,12 @@ class MenuItem(BaseModel):
     serving_time: Optional[time]  # Serving time period
     cooking_time: Optional[time]  # Estimated cooking time
 
-    def __init__(self, name, price, category, image_name="", discount=0, serving_time=time(0), cooking_time=time(0)):
+    def __init__(self, name, price, category, image_name="", discount=0, status=14, serving_time=time(0), cooking_time=time(0)):
         self.name = name
         self.price = price
         self.category = category
         self.image_name = image_name
         self.discount = discount
+        self.status = status
         self.serving_time = serving_time
         self.cooking_time = cooking_time
