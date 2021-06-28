fc34dccee5cad27957e58f0ba58cd346c6e7a430
Reza Gh <r.msc.student93@gmail.com>
2021-06-29 00:29:43 +0430

    updating url for post request, delete function modified

--- static/js/menu_items.js
+++ static/js/menu_items.js
@@ -7,14 +7,13 @@ $(document).ready(function () {
         $inputs.each(function () {
             values[this.name] = $(this).val();
         });
-
         $("#row_h6_" + index).text(values["name"])
         $("#row_span_" + index).text(values["price"])
         var data = {"action": "update", "name": values["name"], "price": values["price"]};
         $.ajax({
             type: 'POST',
             contentType: 'application/json',
-            url: '/cashier/menu',
+            url: '/cashier/' + $(this).attr("data-user_id") + '/menu',
             dataType: 'json',
             data: JSON.stringify(data),
             success: function (result) {
@@ -29,17 +28,22 @@ $(document).ready(function () {
     $(".delete_form").submit(function (event) {
         event.preventDefault();
         var index = $(this).attr("data-index")
-        var data = {"action": "delete", "row": index};
+        var $inputs = $('#edit_form' + index + ' :input');
+        var values = {};
+        $inputs.each(function () {
+            values[this.name] = $(this).val();
+        });
+        var data = {"action": "delete", "row": index, "name": values["name"]};
         $.ajax({
             type: 'POST',
             contentType: 'application/json',
-            url: '/cashier/menu',
+            url: '/cashier/' + $(this).attr("data-user_id") + '/menu',
             dataType: 'json',
             data: JSON.stringify(data),
             success: function (result) {
                 console.log("ok", result);
             }, error: function (result) {
-                console.log("Not ok", result);
+                console.log(result);
             }
         });
         $("#delete" + index).modal('hide');
@@ -56,13 +60,11 @@ $(document).ready(function () {
         $inputs.each(function () {
             values[this.name] = $(this).val();
         });
-        console.log(values)
         var data = {"action": "add", "name": values["new_name"], "price": values["new_price"]};
-        console.log(data)
         $.ajax({
             type: 'POST',
             contentType: 'application/json',
-            url: '/cashier/menu',
+            url: '/cashier/' + $(this).attr("data-user_id") + '/menu',
             dataType: 'json',
             data: JSON.stringify(data),
             success: function (result) {
