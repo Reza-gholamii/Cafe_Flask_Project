9a7deb2d89db7cf90e8a4ed0dcb1ea68f455557b
SepehrBazyar <bazyar.sepehr@gmail.com>
2021-06-29 17:39:51 +0430

    finish table model and add name is class attr

--- model/categories.py
+++ model/categories.py
@@ -9,10 +9,11 @@ class Category(BaseModel):
     Model of Category and SubCategories in DataBase
     """
 
+    name = "categories"
     title: str
 
     def __init__(self, title, root: str = None):
         self.title = title
         if root:
-            self.root = db_manager.get_id("categories", field=root)
-        db_manager.create("categories", self)
+            self.root = db_manager.get_id(self.name, field=root)
+        db_manager.create(self.name, self)
--- model/tables.py
+++ model/tables.py
@@ -9,6 +9,7 @@ class Table(BaseModel):
     Information About Tables of in the Cafe for Example Cafe Position Space and ...
     """
 
+    name = "tables"
     number: int
     capacity: int
     position_space: str
@@ -18,4 +19,12 @@ class Table(BaseModel):
         self.capacity = capacity
         self.position_space = position_space
         self.status = db_manager.get_id("statuses", title=status)
-        self.number = db_manager.create("tables", self)
+        self.number = db_manager.create(self.name, self)
+
+    def change_status(self, status="full"):
+        """
+        Method for Change Status of Model from empty to full or Upside Down
+        """
+
+        code = db_manager.get_id("statuses", title=status)
+        db_manager.update(self.name, id=self.number, status=code)
