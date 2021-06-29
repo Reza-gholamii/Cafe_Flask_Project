7523a784c792be61b7f341f7c1dbb1aadbeade7b
Reza Gh <r.msc.student93@gmail.com>
2021-06-29 21:40:06 +0430

    modal updating status added

--- static/js/lists.js
+++ static/js/lists.js
@@ -2,16 +2,22 @@ function change_status(status) {
     let class_name = status.classList[1]
     let target_name = status.classList[2]
     let target_column = document.getElementById('status' + target_name.slice(-1))
+    let target_modal = document.getElementById('modal_status' + target_name.slice(-1))
     if (class_name === 'icon_refresh') {
         target_column.innerText = 'جدید'
+        target_modal.innerText = 'جدید'
     } else if (class_name === 'icon_cook') {
         target_column.innerText = 'در حال پخت'
+        target_modal.innerText = 'در حال پخت'
     } else if (class_name === 'icon_serve') {
         target_column.innerText = 'سرو شده'
+        target_modal.innerText = 'سرو شده'
     } else if (class_name === 'icon_check') {
         target_column.innerText = 'پرداخت شده'
+        target_modal.innerText = 'پرداخت شده'
     } else if (class_name === 'icon_trash') {
         target_column.innerText = 'کنسل شده'
+        target_modal.innerText = 'کنسل شده'
     }
 }
 
