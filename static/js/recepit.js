5700adc71e301f8615c1311f54978b4b7bfd84b9
Reza Gh <r.msc.student93@gmail.com>
2021-06-30 21:24:51 +0430

    recepit js added for post ajax call

---
+++ static/js/recepit.js
@@ -0,0 +1,32 @@
+function change_status(status) {
+    var index = status.getAttribute(("data-recepit"))
+    let class_name = status.classList[1]
+    let target_name = status.classList[2]
+    let target_column = document.getElementById(target_name)
+
+    if (class_name === 'icon_refresh') {
+        new_status = 'جدید'
+    } else if (class_name === 'icon_check') {
+        new_status = 'پرداخت شده'
+    } else if (class_name === 'icon_trash') {
+        new_status = 'کنسل شده'
+    }
+    target_column.innerText = new_status
+
+    var data = {"recepit_id": index, "status": new_status};
+
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: '/cashier/' + user_id + '/recepits',
+        dataType: 'json',
+        data: JSON.stringify(data),
+        success: function (result) {
+            console.log("ok", result);
+        }, error: function (result) {
+            console.log(result);
+        }
+    });
+}
+
+
