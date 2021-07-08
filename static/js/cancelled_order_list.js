423e78861ca6e7196d4cf8df9c76721597763ddf
mryazdan <mrrezayazdani@yahoo.com>
2021-07-08 09:24:51 +0430

    cofigure js cancelled order list

--- static/js/cancelled_order_list.js
+++ static/js/cancelled_order_list.js
@@ -1,46 +1,50 @@
-function change_status(status) {
-    var index = status.classList[3].slice(-1)
-    let class_name = status.classList[1]
-    let target_name = status.classList[2]
-    let target_column = document.getElementById('status' + target_name.slice(-1))
-    let target_modal = document.getElementById('modal_status' + target_name.slice(-1))
-    if (class_name === 'icon_refresh') {
-        new_status = 'جدید'
-    } else if (class_name === 'icon_cook') {
-        new_status = 'در حال پخت'
-    } else if (class_name === 'icon_serve') {
-        new_status = 'سرو شده'
-    } else if (class_name === 'icon_check') {
-        new_status = 'پرداخت شده'
-    } else if (class_name === 'icon_trash') {
-        new_status = 'کنسل شده'
+$(document).ready(function () {
+
+    $('a.icon').click(function () {
+        change_status(this)
+    })
+
+    function change_status(status) {
+        var index = status.classList[3].slice(-1)
+        let class_name = status.classList[1]
+        let target_name = status.classList[2]
+        let target_column = document.getElementById('status' + target_name.slice(-1))
+        let target_modal = document.getElementById('modal_status' + target_name.slice(-1))
+        if (class_name === 'icon_refresh') {
+            new_status = 'جدید'
+        } else if (class_name === 'icon_cook') {
+            new_status = 'در حال پخت'
+        } else if (class_name === 'icon_serve') {
+            new_status = 'سرو شده'
+        } else if (class_name === 'icon_check') {
+            new_status = 'پرداخت شده'
+        } else if (class_name === 'icon_trash') {
+            new_status = 'کنسل شده'
+        }
+
+        target_column.innerText = new_status
+        target_modal.innerText = new_status
+
+        let order_id = status.getAttribute("data-order")
+
+        var data = {
+            "order_id": order_id,
+            "status": new_status
+        };
+        $.ajax({
+            type: 'POST',
+            contentType: 'application/json',
+            url: '/cashier/orders/cancelled',
+            dataType: 'json',
+            data: JSON.stringify(data),
+            success: function (result) {
+                console.log("ok", result);
+            }, error: function (result) {
+                console.log(result);
+            }
+        });
     }
 
-    target_column.innerText = new_status
-    target_modal.innerText = new_status
-
-    let order_id = status.getAttribute("data-order")
-
-    var data = {
-        "order_id": order_id,
-        "status": new_status
-    };
-    $.ajax({
-        type: 'POST',
-        contentType: 'application/json',
-        url: '/cashier/' + user_id + '/orders/cancelled',
-        dataType: 'json',
-        data: JSON.stringify(data),
-        success: function (result) {
-            console.log("ok", result);
-        }, error: function (result) {
-            console.log(result);
-        }
-    });
-}
-
-
-$(document).ready(function () {
     $("#searchInput").on("keyup", function () {
         var value = $(this).val();
         $("#table_body tr").filter(function () {
